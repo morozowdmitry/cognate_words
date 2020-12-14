805fd21c1802970ee7dc5f6f992c03f73e32bcf6
k.nosorev <k.nosorev@g.nsu.ru>
2020-12-14 12:34:31 +0700

    prepare dataset

--- baseline/baseline.py
+++ baseline/baseline.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from neural_morph_segm import load_cls
 
 import sys
@@ -29,30 +30,86 @@ def getSubstring(str1, str2):
 def getInput(word1, word2):
     word1, word2 = word1.lower(), word2.lower()
     ans = getRoots([word1, word2])
-    roots = [getBestRoot(a)[0] for a in ans]
-    substr = getSubstring(*roots)
-    if not substr:
-        return {
-            "prefix_1": roots[0],
-            "postfix_1": "",
-            "prefix_2": roots[1],
-            "postfix_2": "",
-            "substr_len": len(substr)
-        }
-    roots_splited = [r.split(substr) for r in roots]
-    return {
-        "prefix_1": roots_splited[0][0],
-        "postfix_1": roots_splited[0][1],
-        "prefix_2": roots_splited[1][0],
-        "postfix_2": roots_splited[1][1],
-        "substr_len": len(substr)
-    }
 
+    roots = [(a1[0], a2[0]) for a1 in ans[0] for a2 in ans[1]]
+
+    res = []
+    
+    for r in roots:
+        substr = getSubstring(*r)
+        if len(r) == 1:
+            print(roots, word1, word2)
+            continue
+        if not substr:
+            res.append({
+                "prefix_1": r[0],
+                "postfix_1": "",
+                "prefix_2": r[1],
+                "postfix_2": "",
+                "substr_len": len(substr)
+            })
+            continue
+        roots_splited = [rot.split(substr) for rot in r]
+        res.append({
+            "prefix_1": roots_splited[0][0],
+            "postfix_1": roots_splited[0][1],
+            "prefix_2": roots_splited[1][0],
+            "postfix_2": roots_splited[1][1],
+            "substr_len": len(substr)
+        })
+    return res
+
+
+res = None
+
+eng_titles = {
+    "prefix_1": 3,
+    "prefix_2": 4,
+    "postfix_1": 5,
+    "postfix_2": 6,
+    "substr_len": 7
+}
+
+for_insert = []
+
+
+def parseRow(row):
+    a = getInput(row[0], row[1])
+    if row[-1] == 1 and len(a) > 1:
+        row[-1] = 2
+    for key in a[0]:
+        row[eng_titles[key]] = a[0][key]
+
+    for idx, _ in enumerate(a[1:]):
+        a[idx]['Корень_x'] = row[0]
+        a[idx]['Корень_y'] = row[1]
+        a[idx]['key'] = row[-1]
+    
+    for_insert.extend(a[1:])
+    return row
 
 if __name__ == "__main__":
     model = load_cls("models/morphemes-3-5-3-memo.json")
-    words = input("Введите два слова: ").split()
 
-    words_list = list(map(str.strip, words))
+    import pandas as pd
 
-    print(getInput(*words_list))
+    data2 = pd.read_csv("data/ds_common_words_utf_8.csv")
+    data2_tmp = pd.read_csv("data/ds_common_words_utf_8.csv")
+    data2['key'] = 0
+    data2_tmp['key'] = 0
+
+    res = data2.merge(data2, on='key',  how='outer').drop("key", 1)
+    res['Y'] = res['Корень_x'] == res['Корень_y']
+    res = res.drop('Корень_x', 1).drop('Корень_y', 1)
+    res['Префикс_1'] = ""
+    res['Префикс_2'] = ""
+    res['Постфикс_1'] = ""
+    res['Постфикс_2'] = ""
+    res['Длина подстроки'] = 0
+    res = res.apply(parseRow, axis=1, raw=True)
+    res.to_csv("data/res.out")
+    # words = input("Введите два слова: ").split()
+
+    # words_list = list(map(str.strip, words))
+    # print(getRoots(words_list))
+    # print(getInput(*words_list))
