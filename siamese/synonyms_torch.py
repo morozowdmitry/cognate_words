b184e5ae958c8e3391dd30b17c22bd2d8e3df4e5
Dmitry <morozov@ruscorpora.ru>
2021-04-17 19:20:32 +0700

    smth

--- siamese/synonyms_torch.py
+++ siamese/synonyms_torch.py
@@ -37,14 +37,15 @@ def might_be_synonyms(w1, w2):
     return total_intersection > 0
 
 
-def get_synonyms(word):
-    synsets = wn.synsets(word)
+def get_synonyms(df_x, all_words):
     d = {'word_1': [], 'word_2': []}
+    word = df_x.iloc[0]['word']
+    synsets = wn.synsets(word)
     if synsets:
-        synonyms = synsets[0].lemma_names()
+        synonyms = [x for x in synsets[0].lemma_names() if x in all_words]
         for synonym in synonyms:
             d['word_1'].append(word)
-            d['word_2'].append(synonym)
+            d['word_2'].append(synonym.lower())
     return pd.DataFrame(d)
 
 
@@ -60,8 +61,12 @@ if __name__ == "__main__":
     print(synsets)
 
     word_strings = np.loadtxt('glove.tokens.strings.txt', dtype=object)
-    df = pd.DataFrame(word_strings, columns=['words'])
+    df = pd.DataFrame(word_strings[:100], columns=['word'])
     print(df.shape)
+
+    syn_df = df.groupby('word', group_keys=False).apply(get_synonyms, word_strings.tolist()).reset_index(drop=True)
+
+    print(syn_df.head(25))
 
 
 
